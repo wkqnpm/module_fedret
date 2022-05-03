b6617949f71e18e9cf2fab11f030d7ddec9c42f8
keep alive <101098347+wkqnpm@users.noreply.github.com>
2022-05-03 23:31:58 +0800

    Update webpack.config.js

--- search/webpack.config.js
+++ search/webpack.config.js
@@ -1,11 +1,3 @@
-/*
- * @Descripttion: 
- * @version: 
- * @Author: wkq
- * @Date: 2022-04-05 15:56:30
- * @LastEditors: Please set LastEditors
- * @LastEditTime: 2022-04-05 17:36:19
- */
 const HtmlWebpackPlugin=require("html-webpack-plugin")
 const {ModuleFederationPlugin}=require("webpack").container
 module.exports={
